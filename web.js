eb47776f70a6e15faaf0dfa3efb82225f10ac665
Chan Ting Hei William <the11060@yahoo.com.hk>
2017-04-11 19:29:58 +0800

    ssl

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  // response.send('Hello World!');
+  res.sendFile('public/static/index1.html' , { root : __dirname});
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8888;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
